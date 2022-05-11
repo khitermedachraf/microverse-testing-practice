eda4a5af9d814801c4432d02bdc30cdc804fef35
KHITER Med Achraf <khiter.med7@gmail.com>
2022-05-11 07:31:44 +0100

    Imlement funtion and pass ntest

--- tests/task4.test.js
+++ tests/task4.test.js
@@ -1,10 +1,10 @@
 const capitalize = require('../task4');
 
-test('expected length for achraf to be 6', () => {
+test('expected the string to be capitalized = "Achraf" ', () => {
   //Arrange
   const str = 'achraf';
   //Act 
   const capitalizedStr = capitalize(str);
   //Assert
-  expect(stringLength(capitalizedStr)).toBe('Achraf');
+  expect(capitalizedStr).toBe('Achraf');
 })
